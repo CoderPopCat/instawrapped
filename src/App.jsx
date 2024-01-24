5428955f3b5fb2ddf112a97dad36cae45ba5be33
CoderPopCat <rguy6085@gmail.com>
2024-01-24 19:00:01 +0400

    Redeploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import dotenv from 'dotenv'
-dotenv.config()
 import './App.css'
 import { useState } from 'react'
 import './navbar.css'
@@ -13,23 +11,17 @@ import {
 import Upload from './components/Upload'
 import Demo from './components/Demo';
 import Guide from './components/Guide';
-import ReactGA from 'react-ga';
-const TRACKING_ID = process.env.GOOGLE_ID;
-ReactGA.initialize(TRACKING_ID);
 
 function App() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
   return (
     <>
-      <Router>
-        <Switch>
-          <Route path='/' exact children={<Upload />} />
-          <Route path='/demo' exact children={<Demo />} />
-          <Route path='/guide' exact children={<Guide />} />
-        </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        <Route path='/' exact children={<Upload />} />
+        <Route path='/demo' exact children={<Demo />} />
+        <Route path='/guide' exact children={<Guide />} />
+      </Switch>
+    </Router>
     </>
   )
 }
